6a5ace643a5327fa141a278251c05acf70fd644f
wnflocal <1278022159@qq.com>
2018-01-23 19:57:57 +0800

    SQL

---
+++ helpserver.sql
@@ -0,0 +1,170 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.5.51 : Database - helpserver
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`helpserver` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `helpserver`;
+
+/*Table structure for table `acceptorder` */
+
+DROP TABLE IF EXISTS `acceptorder`;
+
+CREATE TABLE `acceptorder` (
+  `acceptorderId` int(11) NOT NULL AUTO_INCREMENT COMMENT '接单表id',
+  `accepterId` int(11) DEFAULT NULL COMMENT '接单者id',
+  `orderId` int(11) DEFAULT NULL COMMENT '订单id',
+  `acceptTime` varchar(32) DEFAULT NULL COMMENT '接单时间',
+  `backReason` varchar(50) DEFAULT NULL COMMENT '退单理由',
+  `backTime` varchar(32) DEFAULT NULL COMMENT '退单时间（为空未取消，不空接单中）',
+  `acceptState` int(11) NOT NULL DEFAULT '1' COMMENT '状态（1接单，0取消，2已完成，3发布方取消）',
+  PRIMARY KEY (`acceptorderId`),
+  KEY `accepterId` (`accepterId`),
+  KEY `orderId` (`orderId`),
+  CONSTRAINT `acceptorder_ibfk_1` FOREIGN KEY (`accepterId`) REFERENCES `user` (`userId`),
+  CONSTRAINT `acceptorder_ibfk_2` FOREIGN KEY (`orderId`) REFERENCES `order` (`orderId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `acceptorder` */
+
+/*Table structure for table `collectorder` */
+
+DROP TABLE IF EXISTS `collectorder`;
+
+CREATE TABLE `collectorder` (
+  `collectId` int(11) DEFAULT NULL COMMENT '收藏id',
+  `orderId` int(11) DEFAULT NULL COMMENT '订单id',
+  `collectorId` int(11) DEFAULT NULL COMMENT '收藏者id',
+  `collectTime` varchar(32) DEFAULT NULL COMMENT '收藏时间',
+  KEY `orderId` (`orderId`),
+  KEY `collectorId` (`collectorId`),
+  CONSTRAINT `collectorder_ibfk_1` FOREIGN KEY (`orderId`) REFERENCES `order` (`orderId`),
+  CONSTRAINT `collectorder_ibfk_2` FOREIGN KEY (`collectorId`) REFERENCES `user` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `collectorder` */
+
+/*Table structure for table `feedback` */
+
+DROP TABLE IF EXISTS `feedback`;
+
+CREATE TABLE `feedback` (
+  `feedbackId` int(11) NOT NULL AUTO_INCREMENT COMMENT '反馈表id',
+  `userId` int(11) DEFAULT NULL COMMENT '反馈用户id',
+  `feedbackInfo` varchar(200) DEFAULT NULL COMMENT '反馈信息',
+  `feedbackTime` varchar(32) DEFAULT NULL COMMENT '反馈时间',
+  `reply` varchar(200) DEFAULT NULL COMMENT '管理员回复',
+  `replyTime` varchar(32) DEFAULT NULL COMMENT '回复时间',
+  PRIMARY KEY (`feedbackId`),
+  KEY `userId` (`userId`),
+  CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `feedback` */
+
+/*Table structure for table `identity` */
+
+DROP TABLE IF EXISTS `identity`;
+
+CREATE TABLE `identity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL COMMENT '用户id',
+  `frontPhoto` varchar(150) DEFAULT NULL COMMENT '身份证正面',
+  `backPhoto` varchar(150) DEFAULT NULL COMMENT '身份证反面',
+  `askTime` varchar(32) DEFAULT NULL COMMENT '请求时间',
+  `checkTime` varchar(32) DEFAULT NULL COMMENT '验证时间（通过或失败时间）',
+  `failureReason` varchar(100) DEFAULT NULL COMMENT '失败原因',
+  `checkState` int(11) NOT NULL DEFAULT '0' COMMENT '验证状态（0请求验证，1已通过）',
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`),
+  CONSTRAINT `identity_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `identity` */
+
+/*Table structure for table `order` */
+
+DROP TABLE IF EXISTS `order`;
+
+CREATE TABLE `order` (
+  `orderId` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `senderId` int(11) NOT NULL COMMENT '发布者id',
+  `typeId` int(11) NOT NULL COMMENT '发布类型id（零活、跑腿）',
+  `title` varchar(32) DEFAULT NULL COMMENT '标题',
+  `commission` int(11) DEFAULT '0' COMMENT '佣金',
+  `menAccount` int(11) DEFAULT '1' COMMENT '需求人数',
+  `detailTime` varchar(32) DEFAULT NULL COMMENT '服务具体时间',
+  `expendTime` varchar(32) DEFAULT NULL COMMENT '预计耗时',
+  `serverInfo` varchar(150) DEFAULT NULL COMMENT '服务详情',
+  `area` varchar(80) DEFAULT NULL COMMENT '所在区域（省市县）',
+  `startPoint` varchar(80) DEFAULT NULL COMMENT '起点（街道）',
+  `endPoint` varchar(80) DEFAULT NULL COMMENT '终点（街道）',
+  `sendTime` varchar(32) DEFAULT NULL COMMENT '发布时间',
+  `repealTime` varchar(32) DEFAULT NULL COMMENT '撤单时间',
+  `repealReason` varchar(50) DEFAULT NULL COMMENT '撤单原因',
+  `orderState` int(11) NOT NULL DEFAULT '0' COMMENT '订单状态（0草稿，1发布进行1中，2已结束，3已取消、4管理员禁止该订单）',
+  `callName` varchar(15) DEFAULT NULL COMMENT '联系人',
+  `callPhone` varchar(20) DEFAULT NULL COMMENT '联系电话',
+  PRIMARY KEY (`orderId`),
+  KEY `senderId` (`senderId`),
+  KEY `typeId` (`typeId`),
+  CONSTRAINT `order_ibfk_1` FOREIGN KEY (`senderId`) REFERENCES `user` (`userId`),
+  CONSTRAINT `order_ibfk_2` FOREIGN KEY (`typeId`) REFERENCES `ordertype` (`ordertypeId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `order` */
+
+/*Table structure for table `ordertype` */
+
+DROP TABLE IF EXISTS `ordertype`;
+
+CREATE TABLE `ordertype` (
+  `ordertypeId` int(11) NOT NULL AUTO_INCREMENT COMMENT '类型id',
+  `type` int(11) NOT NULL COMMENT '类型标志（1.2.3）',
+  `typeName` varchar(15) NOT NULL COMMENT '类型名称',
+  PRIMARY KEY (`ordertypeId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `ordertype` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id，自动增长',
+  `phone` varchar(15) NOT NULL COMMENT '手机（登录账号）',
+  `password` varchar(112) NOT NULL COMMENT '密码',
+  `name` varchar(15) DEFAULT NULL COMMENT '姓名',
+  `nickname` varchar(15) DEFAULT NULL COMMENT '昵称',
+  `address` varchar(64) DEFAULT NULL COMMENT '联系地址',
+  `age` int(11) DEFAULT '0' COMMENT '年龄',
+  `sex` int(11) DEFAULT '1' COMMENT '性别（0女，1男）',
+  `userInfo` varchar(150) DEFAULT NULL COMMENT '简介',
+  `headicon` varchar(200) DEFAULT NULL COMMENT '头像',
+  `registerTime` varchar(32) DEFAULT NULL COMMENT '注册时间',
+  `banTime` varchar(32) DEFAULT NULL COMMENT '禁用时间',
+  `payAccount` varchar(32) DEFAULT NULL COMMENT '支付账号',
+  `permission` int(11) NOT NULL DEFAULT '0' COMMENT '权限（0普通用户，1可接单用户，2被禁用用户）',
+  PRIMARY KEY (`userId`),
+  UNIQUE KEY `phone` (`phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`userId`,`phone`,`password`,`name`,`nickname`,`address`,`age`,`sex`,`userInfo`,`headicon`,`registerTime`,`banTime`,`payAccount`,`permission`) values (48,'18219111621','e10adc3949ba59abbe56e057f20f883e','18219111621','18219111621',NULL,0,1,NULL,NULL,'2018-01-22 23:14:39',NULL,NULL,0),(49,'18219111622','e10adc3949ba59abbe56e057f20f883e','18219111622','18219111622',NULL,0,1,NULL,NULL,'2018-01-22 23:15:41',NULL,NULL,0),(50,'18219111623','e10adc3949ba59abbe56e057f20f883e','18219111623','18219111623',NULL,0,1,NULL,NULL,'2018-01-22 23:18:58',NULL,NULL,0);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
